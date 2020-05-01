cf5a6fa2e957a92d30cd0cb49dba837dd711360a
Stephen Hammond <steve.hammond@remotectrl.co>
2020-05-01 18:12:56 -0400

    include test assertion

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,15 +38,31 @@ kotlin {
                 implementation(kotlin("stdlib-common"))
             }
         }
+        commonTest {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
 
         val jvmMain by getting {
             dependencies {
                 implementation(kotlin("stdlib"))
             }
         }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
         val jsMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-js"))
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
             }
         }
     }
